69f8b2aa4e317794a1f7dadf9dbc5204e96b89df
suksien <hahasuksien@yahoo.com.my>
2021-12-17 11:19:26 -0800

    updates

--- masking_spectra.py
+++ masking_spectra.py
@@ -16,16 +16,19 @@ from matplotlib.ticker import AutoMinorLocator
 mosfire_res = 3610 # K-band for 0.7" slit (https://www2.keck.hawaii.edu/inst/mosfire/grating.html)
 fwhm = misc.convert_resolution(mosfire_res).value
 
-"""
-fitsfile_list = ['/Users/suksientie/Research/data_redux/mgii_stack_fits/J0313-1806_stacked_coadd_tellcorr.fits', \
-                 '/Users/suksientie/Research/data_redux/mgii_stack_fits/J1342+0928_stacked_coadd_tellcorr.fits', \
-                 '/Users/suksientie/Research/data_redux/mgii_stack_fits/J0252-0503_stacked_coadd_tellcorr.fits', \
-                 '/Users/suksientie/Research/data_redux/2010_done/Redux/J0038-1527_201024_done/J0038-1527_coadd_tellcorr.fits']
+fitsfile_list = ['/Users/suksientie/Research/data_redux/wavegrid_vel/J0313-1806/vel123_coadd_tellcorr.fits', \
+                     '/Users/suksientie/Research/data_redux/wavegrid_vel/J1342+0928/vel123_coadd_tellcorr.fits', \
+                     '/Users/suksientie/Research/data_redux/wavegrid_vel/J0252-0503/vel12_coadd_tellcorr.fits', \
+                     '/Users/suksientie/Research/data_redux/wavegrid_vel/J0038-1527/vel1_tellcorr_pad.fits']
+qso_namelist = ['J0313-1806', 'J1342+0928', 'J0252-0503', 'J0038-1527']
 qso_zlist = [7.6, 7.54, 7.0, 7.0]
-"""
 
-fitsfile = '/Users/suksientie/Research/data_redux/2010_done/Redux/J0038-1527_201024_done/J0038-1527_coadd_tellcorr.fits'
-qso_z = 7.0
+###########
+indx = 3
+fitsfile = fitsfile_list[indx]
+qso_z = qso_zlist[indx]
+qso_name = qso_namelist[indx]
+
 wave_zeropoint_value = (1 + 6) * 2800
 vel_zeropoint = False # if False, not using wave_zeropoint_value above
 everyn_break = 20
@@ -37,6 +40,7 @@ one_minF_thresh = 0.3
 nbins = 81
 sig_min = 1e-2
 sig_max = 100.0
+###########
 
 def mask_cgm():
 
@@ -45,7 +49,7 @@ def mask_cgm():
     good_ivar = ivar[outmask]
     norm_good_flux = good_flux / fluxfit
     norm_good_std = good_std / fluxfit
-    vel = mutils.obswave_to_vel(good_wave, vel_zeropoint=vel_zeropoint, wave_zeropoint_value=wave_zeropoint_value)
+    vel = mutils.obswave_to_vel_2(good_wave)
 
     # reshaping to be compatible with MgiiFinder
     norm_good_flux = norm_good_flux.reshape((1, len(norm_good_flux)))
@@ -55,44 +59,11 @@ def mask_cgm():
     mgii_tot = MgiiFinder(vel, norm_good_flux, good_ivar, fwhm, signif_thresh, signif_mask_nsigma=signif_mask_nsigma,
                           signif_mask_dv=signif_mask_dv, one_minF_thresh=one_minF_thresh)
 
-    #return mgii_tot, vel, norm_good_flux
-    return mgii_tot, good_wave, norm_good_flux
-
-def plot_chi(mgii_tot, vel, cont_flux):
-
-    s_mask = np.invert(mgii_tot.signif_gpm[0])
-    s_mask_frac = np.sum(s_mask)/len(s_mask)
-    print(s_mask_frac)
-
-    fig, (ax1, ax2) = plt.subplots(2, figsize=(16, 6), sharex=True)
-    fig.subplots_adjust(left=0.1, bottom=0.1, right=0.98, top=0.93, wspace=0, hspace=0.)
-    chi_min, chi_max = -0.2, 230
-
-    ax1.set_title(fitsfile.split('/')[-1].split('_')[0] + ' (z = %0.2f)' % qso_z, fontsize=18)
-    ax1.plot(vel, cont_flux[0], drawstyle='steps-mid', color='k', linewidth=1.5, zorder=1)
-    #ax1.axhline(y = 1 - one_minF_thresh, color='green', linestyle='dashed', linewidth=1.5)
-    ax1.plot(vel[s_mask], cont_flux[0][s_mask], color='magenta', markersize=7, markeredgewidth=2.5, linestyle='none', alpha=0.7, zorder=2, marker='|')
-    #ax1.plot(vel[f_mask], cont_flux[0][f_mask], color = 'green', markersize = 7, markeredgewidth = 2.5, linestyle = 'none', alpha = 0.7, zorder = 3, marker = '|')
-    ax1.set_ylabel('Raw flux', fontsize=18)
-    #ax1.set_ylim([flux_min, flux_max])
-    ax1.xaxis.set_minor_locator(AutoMinorLocator())
-    ax1.yaxis.set_minor_locator(AutoMinorLocator())
-
-    neg = np.zeros_like(vel) - 100
-    ax2.plot(vel, mgii_tot.signif[0], drawstyle='steps-mid', color='k')
-    ax2.axhline(y=signif_mask_nsigma, color='magenta', linestyle='dashed', linewidth=1.5)
-    ax2.fill_between(vel, neg, mgii_tot.signif[0], where=s_mask, step = 'mid', facecolor = 'magenta', alpha = 0.5)
-    ax2.set_xlabel('v (km/s)', fontsize=18)
-    ax2.set_ylabel(r'$\chi$', fontsize=18)
-    ax2.set_ylim([chi_min, chi_max])
-    ax2.xaxis.set_minor_locator(AutoMinorLocator())
-    ax2.yaxis.set_minor_locator(AutoMinorLocator())
-
-    plt.show()
+    return mgii_tot, vel, good_wave, norm_good_flux
 
 def plot_both(mgii_tot, vel, cont_flux):
 
-    # "vel" variable in practicel can also be wavelength
+    # "vel" variable in practice can also be wavelength
     f_mask = np.invert(mgii_tot.flux_gpm[0])
     s_mask = np.invert(mgii_tot.signif_gpm[0])
 
@@ -105,29 +76,34 @@ def plot_both(mgii_tot, vel, cont_flux):
     flux_min, flux_max = 0.2, 1.9
     chi_min, chi_max = -0.2, 23
 
-    ax1.set_title(fitsfile.split('/')[-1].split('_')[0] + ' (z = %0.2f)' % qso_z, fontsize=18)
+    ax1.set_title(qso_name + ' (z = %0.2f)' % qso_z, fontsize=18)
     ax1.plot(vel, cont_flux[0], drawstyle='steps-mid', color='k', linewidth=1.5, zorder=1)
-    ax1.axhline(y = 1 - one_minF_thresh, color='green', linestyle='dashed', linewidth=1.5)
+    ax1.axhline(y = 1 - one_minF_thresh, color='green', linestyle='dashed', linewidth=1.5, label=r'$1 - \rm{F} = %0.1f$' % one_minF_thresh)
     ax1.plot(vel[s_mask], cont_flux[0][s_mask], color='magenta', markersize=7, markeredgewidth=2.5, linestyle='none', alpha=0.7, zorder=2, marker='|')
     ax1.plot(vel[f_mask], cont_flux[0][f_mask], color = 'green', markersize = 7, markeredgewidth = 2.5, linestyle = 'none', alpha = 0.7, zorder = 3, marker = '|')
     ax1.set_ylabel(r'$F_{\mathrm{norm}}$', fontsize=18)
+    ax1.set_xlim([vel.min(), vel.max()])
     ax1.set_ylim([flux_min, flux_max])
     ax1.xaxis.set_minor_locator(AutoMinorLocator())
     ax1.yaxis.set_minor_locator(AutoMinorLocator())
+    ax1.legend(fontsize=13)
 
     neg = np.zeros_like(vel) - 100
     ax2.plot(vel, mgii_tot.signif[0], drawstyle='steps-mid', color='k')
-    ax2.axhline(y=signif_mask_nsigma, color='magenta', linestyle='dashed', linewidth=1.5)
+    ax2.axhline(y=signif_mask_nsigma, color='magenta', linestyle='dashed', linewidth=1.5, label=r'$\chi$ = %d' % signif_mask_nsigma)
     ax2.fill_between(vel, neg, mgii_tot.signif[0], where=s_mask, step = 'mid', facecolor = 'magenta', alpha = 0.5)
     #ax2.set_xlabel('v (km/s)', fontsize=18)
     ax2.set_xlabel('obs wavelength (A)', fontsize=18)
     ax2.set_ylabel(r'$\chi$', fontsize=18)
+    ax2.set_xlim([vel.min(), vel.max()])
     ax2.set_ylim([chi_min, chi_max])
     ax2.xaxis.set_minor_locator(AutoMinorLocator())
     ax2.yaxis.set_minor_locator(AutoMinorLocator())
+    ax2.legend(fontsize=13)
 
     plt.tight_layout()
     plt.show()
+
 
 ####################### old scripts
 def mask_cgm_old():
@@ -153,3 +129,35 @@ def mask_cgm_old():
     mgii_tot = MgiiFinder(vel, cont_flux, ivar, fwhm, signif_thresh, signif_mask_nsigma=signif_mask_nsigma, signif_mask_dv=signif_mask_dv, one_minF_thresh=one_minF_thresh)
 
     return mgii_tot, vel, cont_flux
+
+def plot_chi(mgii_tot, vel, cont_flux):
+    # (12/17/21) obsolete...? plot_both is better?
+    s_mask = np.invert(mgii_tot.signif_gpm[0])
+    s_mask_frac = np.sum(s_mask)/len(s_mask)
+    print(s_mask_frac)
+
+    fig, (ax1, ax2) = plt.subplots(2, figsize=(16, 6), sharex=True)
+    fig.subplots_adjust(left=0.1, bottom=0.1, right=0.98, top=0.93, wspace=0, hspace=0.)
+    chi_min, chi_max = -0.2, 230
+
+    ax1.set_title(qso_name + ' (z = %0.2f)' % qso_z, fontsize=18)
+    ax1.plot(vel, cont_flux[0], drawstyle='steps-mid', color='k', linewidth=1.5, zorder=1)
+    #ax1.axhline(y = 1 - one_minF_thresh, color='green', linestyle='dashed', linewidth=1.5)
+    ax1.plot(vel[s_mask], cont_flux[0][s_mask], color='magenta', markersize=7, markeredgewidth=2.5, linestyle='none', alpha=0.7, zorder=2, marker='|')
+    #ax1.plot(vel[f_mask], cont_flux[0][f_mask], color = 'green', markersize = 7, markeredgewidth = 2.5, linestyle = 'none', alpha = 0.7, zorder = 3, marker = '|')
+    ax1.set_ylabel('Raw flux', fontsize=18)
+    #ax1.set_ylim([flux_min, flux_max])
+    ax1.xaxis.set_minor_locator(AutoMinorLocator())
+    ax1.yaxis.set_minor_locator(AutoMinorLocator())
+
+    neg = np.zeros_like(vel) - 100
+    ax2.plot(vel, mgii_tot.signif[0], drawstyle='steps-mid', color='k')
+    ax2.axhline(y=signif_mask_nsigma, color='magenta', linestyle='dashed', linewidth=1.5)
+    ax2.fill_between(vel, neg, mgii_tot.signif[0], where=s_mask, step = 'mid', facecolor = 'magenta', alpha = 0.5)
+    ax2.set_xlabel('v (km/s)', fontsize=18)
+    ax2.set_ylabel(r'$\chi$', fontsize=18)
+    ax2.set_ylim([chi_min, chi_max])
+    ax2.xaxis.set_minor_locator(AutoMinorLocator())
+    ax2.yaxis.set_minor_locator(AutoMinorLocator())
+
+    plt.show()
