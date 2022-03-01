0d5cd316e88ce4828454b444603ebd378989d290
suksien <hahasuksien@yahoo.com.my>
2022-03-01 15:23:01 -0800

    update to sh script

--- test_compute_model.sh
+++ test_compute_model.sh
@@ -2,10 +2,10 @@
 now=`date`
 echo "Start: $now"
 
-python /home/sstie/MgII_forest/compute_model_grid.py \
+python /home/sstie/MgII_forest/compute_model_grid_new.py \
 --nproc 5 --fwhm 90 --samp 3 \
 --vmin 10 --vmax 2000 --dv 100 \
---ncopy 1000 --ncovar 1000000 --nmock 500 --seed 9251761 \
+--ncopy 1000 --seed 9251761 \
 --logZmin -6.0 --logZmax -2.0 --nlogZ 3 \
 --cgm_masking
 
